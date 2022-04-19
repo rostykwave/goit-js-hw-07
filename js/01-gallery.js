c5b63fb840d23903c002bdf7900a921c142e912c
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-19 15:21:02 +0300

    gallery 1 almost done
    ///Як блокувати перехід властивостіі href тегу 'a' при кліку, не видаляючи його

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
+// Крок 1
 const galleryContainer = document.querySelector('.gallery');
-
-// console.log(galleryContainer);
-
 ////Створення розмітки з файлу gallery-items
-const cardsMarkup = createCarmsMarkup(galleryItems);
+const cardsMarkup = createCardsMarkup(galleryItems);
 ////Вставлення розміки
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
+////Крок 2
+galleryContainer.addEventListener('click', onImageClick);
+
+
+
+
+// Функції
 ////Фунція, яка створює розмітку збудь-якого масивву об'єтків
-function createCarmsMarkup(images) {
+function createCardsMarkup(images) {
     return images
         .map(({ preview, original, descrition }) => {
             // console.log(image.description);
             return `
 <div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+  <a class="gallery__link">
     <img
       class="gallery__image"
       src="${preview}"
@@ -32,6 +37,22 @@ function createCarmsMarkup(images) {
         })
         .join('');  
 }
-
 ////Join() потрібна для об'єднання елементів масиву, створеного за допомогою map, в один рядок///
+////href="${original}"
 
+function onImageClick(e) {
+  console.log(e.target.nodeName);
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  console.log(e.target.getAttribute('data-source'));
+  const originalImageSource = e.target.getAttribute('data-source');
+
+  basicLightbox.create(`
+		<img src="${originalImageSource}">
+	`).show()
+
+}
+
+
+///Як блокувати перехід властивостіі href тегу a при кліку, не видаляючи його
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const markup = makeMarkup(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', markup )
+
+function makeMarkup(images) {
+    return images
+        .map(({ preview, original, description }) => {
+            return `
+        <a class="gallery__item" href="${original}">
+  <img 
+  class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>
+`;
+        })
+        .join('');
+};
